9acef1d1b2e9debf0e6f4309394fbfda1ac4c70a
Mahad <mahad852@gmail.com>
2024-03-31 01:39:52 -0400

    fixing duplicate key error in _base_ (config/_base_/datasets)

--- config/_base_/dataset/DEFAULT_TEST_LOOK_TWICE.py
+++ config/_base_/dataset/DEFAULT_TEST_LOOK_TWICE.py
@@ -6,17 +6,17 @@ POINT_TEST_COMMON_CFG_LOOK_TWICE = dict(
 
 
 DEFAULT_TEST_LOOK_TWICE = dict(
-    POINT_TWICE_oq_b_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
-    POINT_TWICE_oq_p_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
-    POINT_TWICE_sq_b_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
-    POINT_TWICE_sq_p_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
-    POINT_TWICE_gq_b_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
-    POINT_TWICE_gq_p_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
+    POINT_LOOK_TWICE_oq_b_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
+    POINT_LOOK_TWICE_oq_p_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
+    POINT_LOOK_TWICE_sq_b_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
+    POINT_LOOK_TWICE_sq_p_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
+    POINT_LOOK_TWICE_gq_b_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
+    POINT_LOOK_TWICE_gq_p_val=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_val.jsonl'),
 
-    POINT_TWICE_oq_b_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
-    POINT_TWICE_oq_p_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
-    POINT_TWICE_sq_b_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
-    POINT_TWICE_sq_p_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
-    POINT_TWICE_gq_b_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
-    POINT_TWICE_gq_p_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
+    POINT_LOOK_TWICE_oq_b_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
+    POINT_LOOK_TWICE_oq_p_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='oq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
+    POINT_LOOK_TWICE_sq_b_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
+    POINT_LOOK_TWICE_sq_p_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='sq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
+    POINT_LOOK_TWICE_gq_b_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-b', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
+    POINT_LOOK_TWICE_gq_p_test=dict(**POINT_TEST_COMMON_CFG_LOOK_TWICE, version='gq-p', filename='{{fileDirname}}/../../../../data/pointQA_twice_test.jsonl'),
 )
