148504e7b61918d4be8ba5721ef15a7fc0e276ab
Mahad <mahad852@gmail.com>
2024-03-31 11:44:12 -0400

    attempt to fix bbox targets issue

--- mllm/dataset/process_function/box_process_function.py
+++ mllm/dataset/process_function/box_process_function.py
@@ -127,7 +127,7 @@ class BoxFormatter:
 
     def __call__(self, sentence: str, bboxes_seq: BoxesSeq) -> str:
         all_box = self.bboxes_token_pat.findall(sentence)
-        assert len(all_box) == len(bboxes_seq), f"not match. sentence: {sentence}. boxes:{bboxes_seq}"
+        assert len(all_box) == len(bboxes_seq), f"not match. sentence: {sentence}. boxes:{bboxes_seq}, len all boxes: {len(all_box)}, all_boxes: {all_box}"
         if len(all_box) == 0:
             return sentence
         bboxes_strs = [self.format_box(bboxes) for bboxes in bboxes_seq]
