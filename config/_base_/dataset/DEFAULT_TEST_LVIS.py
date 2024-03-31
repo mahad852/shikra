9f49e4420a2fcd4a3b347e5c17e231dc2c876068
Mahad <mahad852@gmail.com>
2024-03-31 05:37:21 -0400

    initial code for lvis dataset (multi object eval)

---
+++ config/_base_/dataset/DEFAULT_TEST_LVIS.py
@@ -0,0 +1,13 @@
+LVIS_TEST_CONFIG = dict(
+    type='LVISDataset',
+    image_folder='/datasets/MSCOCO17/val2017',
+    template_file=r"{{fileDirname}}/template/lvis.json",
+)
+
+
+DEFAULT_TEST_LVIS = dict(
+    LVIS_TEST=dict(
+        **LVIS_TEST_CONFIG,
+        filename=r'{{fileDirname}}/../../../../data/lvis_ann.jsonl',
+    )
+)
---
+++ create_lvis_ann.py
@@ -0,0 +1,54 @@
+import json
+
+with open("/Users/ma649596/Downloads/lvis_v1_val.json", "r") as f:
+    obj = json.load(f)
+
+annotations_dict = obj["annotations"]
+images_dict = obj["images"]
+categories_dict = obj["categories"]
+
+category_id_to_name = {}
+max_cat_id = 0
+
+for i in range(len(categories_dict)):
+    category_id_to_name[categories_dict[i]["id"]] = categories_dict[i]["name"]
+    max_cat_id = max_cat_id if categories_dict[i]["id"] <= max_cat_id else categories_dict[i]["id"]
+
+image_id_to_info = {}
+max_image_id = 0
+
+for i in range(len(images_dict)):
+    image_name = images_dict[i]["coco_url"].split('/')[-1]
+    image_id_to_info[images_dict[i]["id"]] = {"img_path" : "val2017/" + image_name, 
+                                              "height" : images_dict[i]["height"], 
+                                              "width" : images_dict[i]["width"]}
+    max_image_id = max_image_id if images_dict[i]["id"] <= max_image_id else images_dict[i]["id"]
+
+final_res_indexer = [[-1 for _ in range(max_cat_id + 1)] for _ in range(max_image_id + 1)]
+final_res = []
+
+for ann_obj in (annotations_dict):
+    image_id = ann_obj["image_id"]
+    category_id = ann_obj["category_id"]
+
+    if final_res_indexer[image_id][category_id] != -1:
+        final_res[final_res_indexer[image_id][category_id]]["num_objects"] += 1
+        final_res[final_res_indexer[image_id][category_id]]["bboxes"].append(ann_obj["bbox"])
+        continue
+
+    final_res_indexer[image_id][category_id] = len(final_res)
+
+    final_res.append({
+        "image_id" : image_id,
+        "category_id" : category_id,
+        **image_id_to_info[image_id],
+        "category_name" : category_id_to_name[category_id],
+        "bboxes" : [ann_obj["bbox"]],
+        "num_objects" : 1
+    })
+
+with open("../data/lvis_ann.jsonl", "w") as f:
+    for i, res_dict in enumerate(final_res):
+        jout = json.dump(res_dict, f)
+        if i != len(final_res) - 1:
+            f.write("\n")
