1d38b025dcea0e75dfa3255a0f5809497bbec8eb
Mahad <mahad852@gmail.com>
2024-04-03 09:39:04 -0400

    added script to concatenate results from lvis run

---
+++ concatenate_lvis_results.py
@@ -0,0 +1,106 @@
+import json 
+
+precision_file = "../data/lvis_precision_log.txt"
+ann_file = "../data/lvis_small_sample.jsonl"
+
+categories_dict = {}
+with open("../data/lvis_v1_val.json", "r") as f:
+    obj = json.load(f)
+    categories = obj["categories"]
+
+is_category_rare = {}
+
+for category in categories:
+    is_category_rare[category["id"]] = category["instance_count"] <= 10        
+
+images = {}
+categories = {}
+
+precisions = []
+
+with open(precision_file, "r") as f:
+    precisions = [float(line[:-1]) for line in f.readlines()]
+
+
+with open(ann_file, "r") as f:
+    for i, line in enumerate(f.readlines()):
+        obj = json.loads(line)
+
+        if obj["category_id"] not in categories:
+            categories[obj["category_id"]] = {"images" : [obj["img_path"]], "appearances" : 1, "mAP" : precisions[i], "name" : obj["category_name"], "is_category_rare" : is_category_rare[obj["category_id"]]}
+        else:
+            categories[obj["category_id"]]["images"].append(obj["img_path"])
+            categories[obj["category_id"]]["appearances"] += 1
+            categories[obj["category_id"]]["mAP"] += precisions[i]
+
+
+        if obj["image_id"] not in images:
+            images[obj["image_id"]] = {"path" : obj["img_path"], "num_objects" : obj["num_objects"], "precision" : precisions[i] * obj["num_objects"]}
+        else:
+            images[obj["image_id"]]["num_objects"] += obj["num_objects"]
+            images[obj["image_id"]]["precision"] += precisions[i] * obj["num_objects"]
+
+
+
+object_count_precision = {
+    1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0,
+    "6-10" : 0,
+    "11-20" : 0,
+    "20+" : 0
+}
+
+object_count_total = {
+    1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0,
+    "6-10" : 0,
+    "11-20" : 0,
+    "20+" : 0
+}
+
+for img_id in images.keys():
+    img_obj = images[img_id]
+    precision = img_obj["precision"]/img_obj["num_objects"]
+
+    if img_obj["num_objects"] < 6:
+        key = img_obj["num_objects"]
+    elif img_obj["num_objects"] >= 6 and img_obj["num_objects"] <= 10:
+        key = "6-10"
+    elif img_obj["num_objects"] >= 11 and img_obj["num_objects"] <= 20:
+        key = "11-20"
+    else:
+        key = "20+"
+
+    object_count_precision[key] += precision
+    object_count_total[key] += 1
+
+print("Mean Average Precision (mAP) by the number of objects in the image:")
+for obj_count in object_count_precision.keys():
+    print("Count:", obj_count, "Precision:", object_count_precision[obj_count]/object_count_total[obj_count])
+
+
+rare_precision = 0
+rare_count = 0
+
+common_count = 0
+common_precision = 0
+
+
+for category_id in categories.keys():
+    categories[category_id]["mAP"] /= categories[category_id]["appearances"]
+
+    if is_category_rare[category_id]:
+        rare_count += 1
+        rare_precision += categories[category_id]["mAP"]
+    else:
+        common_count += 1
+        common_precision += categories[category_id]["mAP"]
+
+print("-----------------------------------------------------------")
+
+print("Precision for Rare objects:", rare_precision/rare_count)
+print("Precision for Common objects:", common_precision/common_count)
+
+with open("../data/lvis_by_class.jsonl", "w") as f:
+    for i, k in enumerate(categories.keys()):
+        jout = json.dump(categories[k], f)
+        if i != len(categories) - 1:
+            f.write("\n")
